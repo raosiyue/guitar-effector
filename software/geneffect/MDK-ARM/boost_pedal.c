85a5192681b3d3229acffbdd672d9bee05d4ad2f
raosiyue <raosiyue@baidu.com>
2024-01-08 09:31:14 +0800

    new project

--- software/geneffect/MDK-ARM/boost_pedal.c
+++
@@ -1,2 +0,0 @@
-#include "effect_chain.h"
-
--- software/geneffect/MDK-ARM/geneffect/driver_lcd.d
+++
@@ -1,3 +0,0 @@
-geneffect\driver_lcd.o: ..\self_driver\LCD\driver_lcd.c
-geneffect\driver_lcd.o: ..\self_driver\LCD\driver_oled.h
-geneffect\driver_lcd.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
